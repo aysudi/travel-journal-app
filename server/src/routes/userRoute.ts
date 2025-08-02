c43619d3cec1ab4687bf0555a908a6e97a3acb97
Aysu <aysu@Noutbuk-Aysu.local>
2025-08-02 17:22:04 +0400

    feat: added login and register user controllers

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import {
+  deleteUser,
+  getUserByEmail,
+  getUsers,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-// userRouter.get("/", getUsers);
+userRouter.get("/", getUsers);
+userRouter.get("/:email", getUserByEmail);
+userRouter.delete("/:id", deleteUser);
 
 export default userRouter;
