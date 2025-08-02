30be9828badcceeced8f05a71113588a3ab055d3
Aysu <aysu@Noutbuk-Aysu.local>
2025-08-02 14:43:40 +0400

    feat: compile ts file and runing the server

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
