40117c9b0d6c654d106047700b03baf92e7c1bc1
Aysu <aysu@Noutbuk-Aysu.local>
2025-08-02 16:44:52 +0400

    feat: connect mongodb and write config folder

---
+++ server/src/config/config.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT || 3000,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_URL: process.env.DB_URL,
+  GMAIL_USER: process.env.GMAIL_USER,
+  GMAIL_PASS: process.env.GMAIL_PASS,
+  JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
+  JWT_ACCESS_SECRET_KEY: process.env.JWT_ACCESS_SECRET_KEY,
+  SERVER_URL: process.env.SERVER_URL || "http://localhost:3000",
+  CLIENT_URL: process.env.CLIENT_URL,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+};
