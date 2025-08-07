c19c864e123f9dc31788c685b91c03bd9e938f9e
Aysu <aysu@Noutbuk-Aysu.local>
2025-08-07 23:36:05 +0400

    feat: created controllers and routes for travel lists

--- server/src/routes/travelListRoute.ts
+++ server/src/routes/travelListRoute.ts
@@ -1,7 +1,45 @@
 import express from "express";
+import {
+  getAllTravelLists,
+  getTravelListById,
+  getOwnedTravelLists,
+  getCollaboratingTravelLists,
+  getPublicTravelLists,
+  createTravelList,
+  updateTravelList,
+  deleteTravelList,
+  addCollaborator,
+  removeCollaborator,
+  uploadCoverImage,
+  duplicateTravelList,
+} from "../controllers/travelListController";
+import { authenticateToken } from "../middlewares/authMiddleware";
+import uploadMiddleware from "../middlewares/uploadMiddleware";
 
 const travelListRouter = express.Router();
 
-// travelListRouter.get("/", getTravelLists);
+travelListRouter.get("/public", getPublicTravelLists);
+
+travelListRouter.use(authenticateToken);
+
+travelListRouter.get("/", getAllTravelLists);
+travelListRouter.get("/owned", getOwnedTravelLists);
+travelListRouter.get("/collaborating", getCollaboratingTravelLists);
+travelListRouter.get("/:id", getTravelListById);
+
+travelListRouter.post("/", createTravelList);
+travelListRouter.put("/:id", updateTravelList);
+travelListRouter.delete("/:id", deleteTravelList);
+
+travelListRouter.post("/:id/collaborators", addCollaborator);
+travelListRouter.delete("/:id/collaborators", removeCollaborator);
+
+travelListRouter.post(
+  "/:id/cover-image",
+  ...uploadMiddleware("travel-lists"),
+  uploadCoverImage
+);
+
+travelListRouter.post("/:id/duplicate", duplicateTravelList);
 
 export default travelListRouter;
